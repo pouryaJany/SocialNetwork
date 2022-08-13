43b9d7ea1abe519a7f2cc19ff9db34bd09879d0b
pouryaJany <p.janparvar@yahoo.com>
2022-08-13 22:21:03 +0430

    updated urls.py of home app

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import HomeView
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', HomeView.as_view(), name='home')
 ]
