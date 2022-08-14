e6fa24326732790ea13a2a264d9973053a769955
pouryaJany <p.janparvar@yahoo.com>
2022-08-14 18:27:19 +0430

    added widget to username form in register_page.html

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,9 @@ from django import forms
 
 
 class RegistrationUserForm(forms.Form):
-    username = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'pourya'}))
+
     email = forms.EmailField()
     password = forms.CharField()
