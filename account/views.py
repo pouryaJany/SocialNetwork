2889969e04eeba8d6b3a36e7a5dce9a92326d497
pouryaJany <p.janparvar@yahoo.com>
2022-08-15 11:56:59 +0430

    register logic

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import RegistrationUserForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -13,3 +15,13 @@ class RegisterUserView(View):
             'form': form
         }
         return render(request, 'account/register_page.html', context)
+
+    def post(self, request):
+        form = RegistrationUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["username"], cd["email"], cd["password"])
+            messages.success(request, "you registered successfully", "success")
+            return redirect('home:home')
+        else:
+            return render(request, 'account/register_page.html', {'form': form})
