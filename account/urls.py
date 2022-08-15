f6f94cafa8147e1da45ee0f2e24c42cffb96568c
pouryaJany <p.janparvar@yahoo.com>
2022-08-15 19:29:42 +0430

    login url

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, LoginUserView
 
 app_name = 'account'
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register')
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login')
 ]
