7e3795a6b7051e48ae2be7644baaf4dff0699021
pouryaJany <p.janparvar@yahoo.com>
2022-08-16 11:39:43 +0430

    logout url

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView
+from .views import RegisterUserView, LoginUserView, LogoutUserView
 
 app_name = 'account'
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
-    path('login/', LoginUserView.as_view(), name='login')
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', LogoutUserView.as_view(), name='logout')
+
 ]
