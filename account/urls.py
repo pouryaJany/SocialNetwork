e4977c3449fe2ebec6a9ff923f68227d3efa74f6
pouryaJany <p.janparvar@yahoo.com>
2022-08-16 17:22:35 +0430

    url of profile view

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView, LogoutUserView
+from .views import RegisterUserView, LoginUserView, LogoutUserView, UserProfileView
 
 app_name = 'account'
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('logout/', LogoutUserView.as_view(), name='logout')
-
+    path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile')
 ]
