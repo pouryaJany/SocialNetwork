6a0954b3e138edbfebd327ea7309240dc9d42733
pouryaJany <p.janparvar@yahoo.com>
2022-08-30 15:29:21 +0430

    created show detail post view

--- post/views.py
+++ post/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Post
+
+
 # Create your views here.
 
 
@@ -13,3 +15,16 @@ class ShowAllPost(View):
         }
         return render(request, 'post/all_post.html', context)
 
+
+class ShowDetailPost(View):
+    @staticmethod
+    def get(request, id, slug):
+        post = Post.objects.get(id=id, slug=slug)
+        images = [post.image.image1, post.image.image2, post.image.image3, post.image.image4, post.image.image5]
+        print(images)
+        context = {
+            'post': post,
+            'images': images
+        }
+
+        return render(request, 'post/detail_post.html', context)
