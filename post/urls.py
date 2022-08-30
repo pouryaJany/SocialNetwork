177c1372dcc372d0f42b4200002993a0b27b002c
pouryaJany <p.janparvar@yahoo.com>
2022-08-30 15:30:28 +0430

    created our view url

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('all-post/', views.ShowAllPost.as_view(), name='all-post')
+    path('all-post/', views.ShowAllPost.as_view(), name='all-post'),
+    path('detail-post/<int:id>/<slug:slug>/', views.ShowDetailPost.as_view(), name='detail-post')
 ]
