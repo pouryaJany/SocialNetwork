06a4f88f7ddeac7768d800827838d29bb2d85107
pouryaJany <p.janparvar@yahoo.com>
2022-08-30 15:34:49 +0430

    add static path for showing pictures

--- SocialNetwork/urls.py
+++ SocialNetwork/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
     path('account/', include('account.urls', namespace='account')),
     path('post/', include('post.urls', namespace='post'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
