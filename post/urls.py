2c0ed1f31c250c8f1766a599d06225a01157eeb8
pouryaJany <p.janparvar@yahoo.com>
2022-09-17 17:30:01 +0430

    url of delete post created

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,6 @@ app_name = 'post'
 
 urlpatterns = [
     path('all-post/', views.ShowAllPost.as_view(), name='all-post'),
-    path('detail-post/<int:id>/<slug:slug>/', views.ShowDetailPost.as_view(), name='detail-post')
+    path('detail-post/<int:id>/<slug:slug>/', views.ShowDetailPost.as_view(), name='detail-post'),
+    path('delete-post/<int:post_id>/', views.DeletePost.as_view(), name='delete-post')
 ]
