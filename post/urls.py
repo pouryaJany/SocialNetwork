da4066959331eb358626c8086aadbddde6719a14
pouryaJany <p.janparvar@yahoo.com>
2022-09-21 07:31:26 +0430

    urls of our views

--- post/urls.py
+++ post/urls.py
@@ -6,5 +6,7 @@ app_name = 'post'
 urlpatterns = [
     path('all-post/', views.ShowAllPost.as_view(), name='all-post'),
     path('detail-post/<int:id>/<slug:slug>/', views.ShowDetailPost.as_view(), name='detail-post'),
-    path('delete-post/<int:post_id>/', views.DeletePost.as_view(), name='delete-post')
+    path('delete-post/<int:post_id>/', views.DeletePost.as_view(), name='delete-post'),
+    path('update-post/<int:post_id>/', views.UpdatePost.as_view(), name='update-post'),
+    path('create-post/', views.CreatePost.as_view(), name='create-post')
 ]
