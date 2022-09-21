6c85bf22d57ce1963b650dac20d40ac4fede2cb4
pouryaJany <p.janparvar@yahoo.com>
2022-09-21 07:58:51 +0430

    EMAIL_HOST_PASSWORD

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -134,3 +134,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'janparvarpourya@gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'vhwwzfzbntxurrwl'
